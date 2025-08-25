a43b9df6124fa13280059091a844bc69a1988193
unknown <ricardo9805@javerianacali.edu.co>
2025-08-25 14:03:55 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+// Define la función App() para que retorne el JSX de la tarjeta.
+function App() {
+  return (
+    <div className="card">
+      <h1>Ricardo Perez y Samuel Vergara</h1>
+      <p>Ingenieros de sistemas en formacion</p>
+    </div>
+  );
+}
+
+export default App;
+
